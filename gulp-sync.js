abb007ece7a1b620a76f78d40da6739be890b5b8
iansun <lauer3912@gmail.com>
2017-07-26 16:04:24 +0800

    update with gulp-sync.js

--- gulp-sync.js
+++ gulp-sync.js
@@ -12,65 +12,78 @@ const sysPath = require('path')
 const ignoreFils = ['CNAME','.git', '.idea', '.gitignore', '.npmignore', '.travis.yml', '.travis', 'node_modules']
 var g_sync_dir = ""
 
-gulp.task('sync:git:pull:xxxx', function(cb){
-  git.pull('origin', ['master'], {cwd:g_sync_dir}, function(err){
+gulp.task('sync:git:pull', function(cb){
+  git.pull('origin', ['master'], {cwd:g_sync_dir, quiet:false}, function(err){
       if (err) throw err;
       cb (err)
   })
 })
 
-gulp.task('sync:files:xxxx', ['sync:git:pull:xxxx'], function(){
+gulp.task('sync:files', ['sync:git:pull'], function(){
    fileSync('public', g_sync_dir, {
      ignore: ignoreFils
    })
 })
 
-gulp.task('sync:git:add:xxxx', ['sync:files:xxxx'], function(){
+gulp.task('sync:git:add', ['sync:files'], function(){
   return  gulp.src(g_sync_dir)
     .pipe(git.add({cwd:g_sync_dir, quiet:false}))
 })
 
-gulp.task('sync:git:commit:xxxx', ['sync:git:add:xxxx'], function(){
+gulp.task('sync:git:commit', ['sync:git:add'], function(){
   return  gulp.src(g_sync_dir)
     .pipe(git.commit('sync update commit', {cwd:g_sync_dir, quiet:false}))
 })
 
-gulp.task('sync:xxxx', ['sync:git:commit:xxxx'], function(cb){
-  git.push('origin', ['master'], {cwd:g_sync_dir}, function(err){
+gulp.task('sync:git:push', ['sync:git:commit'], function(cb){
+  git.push('origin', ['master'], {cwd:g_sync_dir, quiet:false}, function(err){
       if (err) throw err;
       cb (err)
   })
 })
 
+gulp.task('sync:gmagon.com', function(cb){
+  g_sync_dir = sysPath.normalize(sysPath.join(__dirname, '../gmagon.com/'))
+  console.log('g_sync_dir=', g_sync_dir)
+
+  if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:git:push')(cb) }
+  else{cb()}
+})
 
 gulp.task('sync:gmagon.cn', function(cb){
   g_sync_dir = sysPath.normalize(sysPath.join(__dirname, '../gmagon.cn/'))
   console.log('g_sync_dir=', g_sync_dir)
 
-  if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:xxxx')(cb) }
+  if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:git:push')(cb) }
   else{cb()}
 })
 
 gulp.task('sync:gmagon.co', function(cb){
-  g_sync_dir = '../gmagon.co/';
-  if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:xxxx')(cb) }
+  g_sync_dir = sysPath.normalize(sysPath.join(__dirname, '../gmagon.co/'))
+  console.log('g_sync_dir=', g_sync_dir)
+
+  if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:git:push')(cb) }
   else{cb()}
 })
 
 gulp.task('sync:gmagon.org', function(cb){
-  g_sync_dir = '../gmagon.org/';
-  if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:xxxx')(cb) }
+  g_sync_dir = sysPath.normalize(sysPath.join(__dirname, '../gmagon.org/'))
+  console.log('g_sync_dir=', g_sync_dir)
+
+  if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:git:push')(cb) }
   else{cb()}
 })
 
 gulp.task('sync:gmagon.net', function(cb){
-  g_sync_dir = '../gmagon.net/';
-  if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:xxxx')(cb) }
+  g_sync_dir = sysPath.normalize(sysPath.join(__dirname, '../gmagon.net/'))
+  console.log('g_sync_dir=', g_sync_dir)
+
+  if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:git:push')(cb) }
   else{cb()}
 })
 
 
-gulp.task('default', gulpSequence('sync:gmagon.cn', 'sync:gmagon.co', 'sync:gmagon.net', 'sync:gmagon.org'))
+gulp.task('default', gulpSequence('sync:gmagon.com', 'sync:gmagon.cn', 'sync:gmagon.co', 'sync:gmagon.net', 'sync:gmagon.org'))
 
 /**
  * 按照文件来执行
