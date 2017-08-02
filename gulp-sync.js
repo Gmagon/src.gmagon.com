b096342d06c89d53ffd6db3f5519bc9295d933bd
Ian sun <lauer3912@gmail.com>
2017-08-02 18:13:07 +0800

    update

--- gulp-sync.js
+++ gulp-sync.js
@@ -2,17 +2,20 @@
 'use strict';
 
 var gulp = require('gulp');
-// var gutil = require('gulp-util');
+var gutil = require('gulp-util');
 var fileSync = require('gulp-file-sync');
 var git = require('gulp-git');
 var gulpSequence = require('gulp-sequence');
 var exec = require('child_process').exec;
+var yaml = require('js-yaml');
 
 const sysFS = require('fs')
 const sysPath = require('path')
+const sysUtil = require('util')
 
 const ignoreFils = ['CNAME','.git', '.idea', '.gitignore', '.npmignore', '.travis.yml', '.travis', 'node_modules']
 var g_sync_dir = ""
+var g_site_url = "https://gmagon.com"
 var enable_sync_files = true
 var enable_clean_or_revert = true
 
@@ -74,6 +77,23 @@ gulp.task('sync:git:push', ['sync:git:commit'], function(cb){
   })
 })
 
+gulp.task('modify:_config.yml', function(cb){
+   var org_configYamlFile = sysPath.normalize(sysPath.join(__dirname, '/gmagon_config.yml'))
+   var dest_configYamlFile = sysPath.normalize(sysPath.join(__dirname, '/_config.yml'))
+
+   var doc = yaml.safeLoad(sysFS.readFileSync(org_configYamlFile, 'utf8'))
+   doc.url = g_site_url
+   gutil.log(sysUtil.inspect(doc, false, 10, true));
+
+   //save file
+   var format_doc = yaml.dump(doc,{
+     lineWidth: 800
+   })
+   gutil.log(format_doc)
+
+   sysFS.writeFileSync(dest_configYamlFile, format_doc, 'utf-8')
+ })
+
 /////////////////////////////////////////////////////////////////////////////////////
 gulp.task('sync:gmagon.com', function(cb){
   g_sync_dir = sysPath.normalize(sysPath.join(__dirname, '../gmagon.com/'))
@@ -117,12 +137,40 @@ gulp.task('sync:gmagon.org', function(cb){
 
 gulp.task('sync:gmagon.net', function(cb){
   g_sync_dir = sysPath.normalize(sysPath.join(__dirname, '../gmagon.net/'))
+  
   console.log('g_sync_dir=', g_sync_dir)
   enable_sync_files = true
   enable_clean_or_revert = true
 
   if (sysFS.existsSync(g_sync_dir)) { gulpSequence('sync:git:push')(cb) }
   else{cb()}
+})
+
+/////////////////////////////////////////////////////////////////////////
+// 修改_config.yaml
+gulp.task('set:gmagon.com', function(cb){
+  g_site_url = "https://gmagon.com"
+  gulpSequence('modify:_config.yml')(cb)
+})
+
+gulp.task('set:gmagon.cn', function(cb){
+  g_site_url = "https://gmagon.cn"
+  gulpSequence('modify:_config.yml')(cb)
+})
+
+gulp.task('set:gmagon.co', function(cb){
+  g_site_url = "https://gmagon.co"
+  gulpSequence('modify:_config.yml')(cb)
+})
+
+gulp.task('set:gmagon.net', function(cb){
+  g_site_url = "https://gmagon.net"
+  gulpSequence('modify:_config.yml')(cb)
+})
+
+gulp.task('set:gmagon.org', function(cb){
+  g_site_url = "https://gmagon.org"
+  gulpSequence('modify:_config.yml')(cb)
 })
 
 //////////////////////////////////////////////////////////////////////////
@@ -136,8 +184,6 @@ gulp.task('sync:src.gmagon.com', function(cb){
   else{cb()}
 })
 
-gulp.task('default', gulpSequence('sync:gmagon.com', 'sync:gmagon.cn', 'sync:gmagon.co', 'sync:gmagon.net', 'sync:gmagon.org'))
-gulp.task('publish', ['sync:src.gmagon.com'])
 
 /**
  * 按照文件来执行
